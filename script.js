5bb8ec28662ded67dc7dd7f3c3021fbf35f556dd
yashkumard6-create <yashkumard6@gmail.com>
2026-07-31 21:04:54 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,52 @@
+function startJourney(){
+
+document.body.innerHTML=`
+
+<div style="
+height:100vh;
+display:flex;
+justify-content:center;
+align-items:center;
+flex-direction:column;
+background:linear-gradient(135deg,#ff758c,#ff7eb3);
+font-family:Arial;
+color:white;
+text-align:center;
+">
+
+<h1 style="font-size:55px;">
+😂 Warning 😂
+</h1>
+
+<p style="font-size:25px;margin-top:20px;">
+This website contains...
+
+❤️ Unlimited Love
+
+🥺 Unlimited Sorry
+
+😂 Little Bit of Drama
+
+Proceed at your own Risk!
+</p>
+
+<button
+onclick="alert('Next Page Coming Soon ❤️')"
+style="
+margin-top:40px;
+padding:15px 40px;
+font-size:22px;
+border:none;
+border-radius:40px;
+cursor:pointer;
+background:white;
+color:#ff4b7d;
+">
+Continue ❤️
+</button>
+
+</div>
+
+`;
+
+}
